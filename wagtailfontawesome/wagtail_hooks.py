433abef075c920c2bf920d0634213bf937d251c2
Vadim-Karpenko3 <vadim@minimuminc.com>
2019-12-02 21:04:06 +0200

    Remove cdn, add new version as a local file

--- wagtailfontawesome/wagtail_hooks.py
+++ wagtailfontawesome/wagtail_hooks.py
@@ -15,7 +15,8 @@ except ImportError:  # fallback for Wagtail <2.0
 
 
 def import_wagtailfontawesome_stylesheet():
-    return format_html('<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />')
+    return format_html('<link rel="stylesheet" href="{}">',
+                       static('wagtailfontawesome/css/wagtailfontawesome.css'))
 
 
 # New Wagtail versions support importing CSS throughout the admin.
